6045fcaf464467de29b498aacbaaef2cb2c173cf
HUSSNIA MOHAMMED <hussniamohammed09@gmail.com>
2025-10-03 14:44:33 -0600

    Add files via upload
    Updated code files

--- prayertodolist.js
+++ prayertodolist.js
@@ -1,9 +1,2 @@
 const username=window.prompt("ASSEALMUALEYKUM WEREHMETULUAHI WEBEREKATUHU PLEASE ENTER YOUR NAME:")
 alert(`welcome to this simple prayer to do list website ${username}!`)
-let prayerTasks = [
-  { id: 1, name: "Fajr", completed: false },
-  { id: 2, name: "Dhuhr", completed: false },
-  { id: 3, name: "Asr", completed: false },
-  { id: 4, name: "Magrib", completed: false },
-  { id: 5, name: "Isha", completed: false }
-];
